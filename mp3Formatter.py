b594e3012a687ae236e7d06b5610adf8fc3bc565
unknown <cammurphy195@gmail.com>
2021-01-03 20:06:22 -0500

    wrote script

---
+++ mp3Formatter.py
@@ -0,0 +1,14 @@
+import os 
+
+directory = r"C:\Users\[path to directory that has mp3 files]"
+# https://datatofish.com/rename-file-python/
+# rename files to get rid of numbers in front of them
+
+for filename in os.listdir(directory):
+    if filename.endswith(".mp3"):
+        if(filename[0].isdigit() and filename[1].isdigit()):
+            new_file_name = filename[3:]
+            old_path_to_file = directory + "/" + filename
+            new_path_to_file = directory + "/" + new_file_name
+            os.rename(old_path_to_file, new_path_to_file)
+            
---
+++ readme.md
@@ -0,0 +1,3 @@
+## MP3 File Formatter
+
+Python script that iterates over mp3 files in a directory and removes track number from name of file
